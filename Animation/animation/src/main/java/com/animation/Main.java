3c6351f40c3739d9096ddbb423a0fb5c5ed650f2
Prathmesh3177 <prathmeshbondar@gmail.com>
2024-07-04 12:52:57 +0530

    JavaFX Animation

---
+++ Animation/animation/src/main/java/com/animation/Main.java
@@ -0,0 +1,9 @@
+package com.animation;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(AnimationDemo.class,args);
+    }
+}
